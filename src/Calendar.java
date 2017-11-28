0f2c58f2ed59e8710cc506989e4778b5971fe5a1
thunderStormGS <34046567+thunderStormGS@users.noreply.github.com>
2017-11-28 10:40:52 -0500

    Update Calendar.java
    Just a quick couple of comments

--- src/Calendar.java
+++ src/Calendar.java
@@ -3,10 +3,10 @@ import java.util.*;
 import java.time.*;
 
 public class Calendar {
-
+	//Initializes the calendar using the ArrayList Data Structure
 	static ArrayList<String> cal = new ArrayList<String>();
 	
-	
+	//A method to add events to the calendar
 	public static boolean addEvent(String newEvent)
 	{
 		cal.add(newEvent);
